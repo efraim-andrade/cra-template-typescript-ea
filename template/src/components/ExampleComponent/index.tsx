6c41b6ff7c9c2cf7ffd6a77dc470ec5387848874
Efraim Andrade <efraim.dev@gmail.com>
2020-03-29 23:17:37 -0300

    feat: 🎸 thats-me
    add me in the project

--- template/src/components/ExampleComponent/index.tsx
+++ template/src/components/ExampleComponent/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Me from '~/assets/thats-me.svg';
+
+import { Container, ThatsMe, Message } from './styles';
 
 interface Props {
   title: string;
@@ -11,7 +13,8 @@ export default function ExampleComponent({ title, color }: Props) {
   return (
     <Container color={color}>
       <h1>
-        {title} <span>(testando chamada de API)</span>
+        {title}
+        <span>(testando a API)</span>
       </h1>
 
       <p>O projeto vem com uma série de padrões pré definidos com:</p>
@@ -91,6 +94,22 @@ export default function ExampleComponent({ title, color }: Props) {
           os caminhos absolutos a partir da pasta src, ex: ~/components
         </li>
       </ul>
+
+      <Message
+        animate={{ y: -100, opacity: 0.5 }}
+        transition={{ duration: 2, ease: 'easeInOut' }}
+      >
+        iae dev!
+      </Message>
+
+      <ThatsMe
+        src={Me}
+        animate={{ y: -180 }}
+        transition={{
+          duration: 2,
+          ease: 'easeOut',
+        }}
+      />
     </Container>
   );
 }
